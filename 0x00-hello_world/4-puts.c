353964a89e21ddf3fb8d3ee2993309483c7c660b
Millennial-Polymath <muchamingotho@gmail.com>
2021-02-22 23:54:05 +0000

    Added a script that prints out a string using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 /*A simple program that prints out a string using puts*/
 #include <stdio.h>
+
+/* testing use of c*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
