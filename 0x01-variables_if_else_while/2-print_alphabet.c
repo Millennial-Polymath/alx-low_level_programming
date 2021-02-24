fa6009198c2b4f79159fb2f29370c229e27a2ee3
Millennial-Polymath <muchamingotho@gmail.com>
2021-02-24 10:39:14 +0000

    Added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point, outputs a to z
+ *
+ * Return: Always 0 (sucess)
+ */
+int  main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
