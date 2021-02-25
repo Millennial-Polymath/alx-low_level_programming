4b78a15c0a15b8e7cc95ef62bbd12ec011001341
Millennial-Polymath <muchamingotho@gmail.com>
2021-02-25 09:43:52 +0000

    Added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints alphabets from a -z, exept q and e
+ *
+ * Return: returns zero
+ **/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+
+		printf("%d\n", n);
+
+	return (0);
+}
