f353ead8b34f04d8443addc80787c4feac71f978
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-02 05:47:36 +0000

    Added 5-sign.c and updated holberton.h file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: The value to be passed to the function
+ *
+ * Return: Returns 1 if n is greator than zero, 0 if zero and -1 if n is less
+ * than zero
+ */
+
+
+
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		return (1);
+	_putchar('+');
+	}
+	if else(n == 0)
+	{
+		return (0);
+		_putchar(0);
+	}
+	else
+	{
+		return (-1);
+	_putchar('-');
+	}
+}
