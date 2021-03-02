2b6189e0e32abfc21a1cb137136e5a8431b3ad20
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-02 05:59:33 +0000

    Fixed the structure of if else statements

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,21 +10,21 @@
 
 
 
-int print_sign(int n);
+int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 	_putchar('+');
+	return (1);
 	}
-	if else(n == 0)
+	else if (n == 0)
 	{
+		_putchar('0');
 		return (0);
-		_putchar(0);
 	}
 	else
 	{
+		_putchar('-');
 		return (-1);
-	_putchar('-');
 	}
 }
