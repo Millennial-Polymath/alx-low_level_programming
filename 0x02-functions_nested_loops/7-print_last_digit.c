cae845e23da3afcc90f7893de6552975954b1b0a
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-03 05:36:35 +0000

    Added 7-last_digit.c and updated the holberton.h file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @x: The value to be passed to the function argument
+ *
+ * Return: returns the value of the last digit
+ */
+
+int print_last_digit (int x)
+{
+	int y;
+
+	if (x >= 0)
+	{
+		y = x % 10;
+		return (y);
+	}
+	else
+	{
+		y = (-1) * (x % 10);
+		return (y);
+	}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -25,6 +25,8 @@ int print_sign(int n);
 
 int _abs(int x);
 
+int print_last_digit(int x);
+
 void jack_bauer(void);
 
 #endif /* HOLBERTON_H */
