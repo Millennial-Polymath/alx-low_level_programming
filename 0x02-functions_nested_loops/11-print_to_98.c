6e5a38d1bb3962ef8e697f596c7156c848da5c3b
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-04 10:32:30 +0000

    Added 11-print_to_98 for some test

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,62 @@
+#include "holberton.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98 followed by a comma
+ * numbers should be separated  by a comma, followed by a space
+ * the first number to be printed should be the number passed to the function
+ * last number should be 98
+ * @n: The value to be passed to the function
+ *
+ * Return: Null
+ */
+
+void print_to_98(int n)
+{
+	int count;
+
+	count = n;
+
+	if (count > 98)
+	{
+		for (count = n; count >= 98; count--)
+		{
+			if (count >= 100)
+			{
+				_putchar(count / 100 + '0');
+				_putchar((count / 10) - 10 + '0');
+				_putchar(count % 10 + '0');
+			}
+			else
+			{
+				_putchar((count / 10) + '0');
+				_putchar((count % 10) + '0');
+			}
+		}
+	}
+	else
+	{
+		for (count = n; count <= 98; count++)
+		{
+			if (count < 10)
+			{
+				_putchar(count + '0');
+			}
+			else if(count < 0)
+			{
+				_putchar('-');
+				_putchar('0'- (count % 10));
+			}
+			else
+			{
+				_putchar((count / 10) + '0');
+				_putchar((count % 10) + '0');
+			}
+		}
+	}
+	if (count != 98)
+	{
+		_putchar(44);
+		_putchar(32);
+		_putchar('\n');
+	}
+}
