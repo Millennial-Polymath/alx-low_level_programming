fb65998df557dd76d0426042186ff15e1e6f1dd6
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-05 14:39:15 +0000

    Added 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - the program starts here, prints 1-100, replacing certain numbers with
+ * words
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int count;
+
+	for (count = 1; count <= 100; count++)
+	{
+		if (((count % 5) == 0) && (count % 3) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if ((count % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((count % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", count);
+		}
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
