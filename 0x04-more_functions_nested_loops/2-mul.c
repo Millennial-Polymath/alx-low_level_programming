8b8a292a8f31359a4c1def3402fa7de58ce3e93e
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-05 04:22:41 +0000

    Fixed betty style errors

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,10 @@
 #include "holberton.h"
-
 /**
  * mul - Multiplies two integers
  * @a: first integer to be passed to the function
  * @b: second integer to be passed to the function
  *
- * Return : returns the product
+ * Return: returns the product
  */
 
 int mul(int a, int b)
@@ -13,5 +12,6 @@ int mul(int a, int b)
 	int result;
 
 	result = a * b;
+
 	return (result);
 }
