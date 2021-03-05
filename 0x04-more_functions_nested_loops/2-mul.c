f9322a935b35917df5092d2359bcd38053fe7f02
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-05 04:07:29 +0000

    added 2-mul.c file and related files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: first integer to be passed to the function
+ * @b: second integer to be passed to the function
+ *
+ * Return : returns the product
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -39,5 +39,8 @@ int _isupper(int c);
 
 int _isdigit(int c);
 
+int mul(int a, int b);
+
+
 
 #endif /* HOLBERTON_H */
