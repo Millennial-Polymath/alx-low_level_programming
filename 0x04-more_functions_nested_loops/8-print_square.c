d3811ffd8c06038900e391cd1ef2f4a243bc39c7
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-05 13:31:23 +0000

    Added 8-main.c and the relate files

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_square.c - prints a square followed by a new line
+ * @size: The number of # that make up the length and width
+ *
+ * Return: Null
+ */
+
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	if (size >= 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
