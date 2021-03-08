545f2e5f0da69115f601fa24b8fc10175643904a
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-08 01:06:00 +0000

    Initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Takers a pointer to an int as a parameter and updates the
+ * value to 98
+ * @n: The variable pointer to an integer
+ *
+ * Return: Null
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
