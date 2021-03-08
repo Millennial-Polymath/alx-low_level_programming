d907ffa80384aac50d19145c7685c146247cd687
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-08 01:20:13 +0000

    Added 1-swap.c and related files

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
+ *
+ * Return: Null
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
