d680621a47520b23333de27dfc017bf0a97ce9fc
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-10 05:12:00 +0000

    Fixed the algorithm

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,18 @@
 void puts2(char *str)
 {
 	int count;
+	int index;
 
 	count = 0;
 	while (str[count] != '\0')
 	{
-		_putchar(str[count]);
-		count = count + 2;
+		count++;
+	}
+	for (index  = 0; index < count; index++)
+	{
+		if (index % 2 != 0)
+			continue;
+		_putchar(*(str + index));
 	}
 	_putchar('\n');
 }
