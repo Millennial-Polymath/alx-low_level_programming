9a25ae8041364996ab1e9371369fc500b0faddd8
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-11 07:53:19 +0000

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point, prints the command first command line argument
+ * @argc: The number of command line arguments to be passed
+ * @argv: The pointer to the array of characters to be passed as arguments
+ *
+ * Return: Null
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc >=0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
