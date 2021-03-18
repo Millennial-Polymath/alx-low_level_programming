79e98c695967d0a0f6698e12db35bffc77c2be14
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-18 22:35:33 +0000

    Addefunction that is gets executed before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * before-main - prints executes before the main function
+ *
+ * Return: Null
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, ");
+	printf("you must allow,\nI bore my house upon my back!\n");
+}
