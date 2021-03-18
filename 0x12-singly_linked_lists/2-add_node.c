bc5175de2c5ce755e972274662d391c76931a19e
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-18 20:40:39 +0000

    Fixed a few errors

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,32 +1,45 @@
 #include "lists.h"
-/**
- * add_node - adds a new node at the top
- * @str: the string to be passed
- * @head: pointer to the pointer to the head node
- * Return: pointer to base address of the new element
- */
 
+/**
+ *_strlen - return the length of a string
+ *@str: string
+ *Return: length
+ */
+int _strlen(const char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ *add_node - adds a new node at the beginning of a list_t list.
+ *@head: linked list
+ *@str: data for new node
+ *Return: address of the new element, or NULL on failure
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
+	if (str == NULL && strdup(str) == NULL)
+		return (NULL);
 
+	new_node = malloc(sizeof(list_t));
 
-	new_node = (list_t *)(malloc(sizeof(list_t)));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	if (str == NULL && strdup(str) == NULL)
-		return (NULL);
+	new_node->len = _strlen(str);
 
-	new_node->next = *head;
 	if (head == NULL)
 	{
 		new_node->next = NULL;
 	}
-	new_node->len = strlen(str);
+	new_node->next = *head;
 	*head = new_node;
-
-	return (*head);
+	return (new_node);
 }
