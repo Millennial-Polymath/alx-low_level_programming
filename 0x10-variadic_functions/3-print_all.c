045fa412844dc5f48ae5fb697dba6d684fb09f1e
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-20 15:18:20 +0000

    Last mandatory task

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,102 @@
+#include "variadic_functions.h"
+
+
+/**
+ * print_char - prints char
+ * @list: list of arguments
+ * Return: NULL
+ */
+
+void print_char(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+/**
+ * print_string - prints string
+ * @list: list of arguments
+ * Return: NULL
+ */
+
+void print_string(va_list list)
+{
+	char *str;
+
+	str = va_arg(list, char *);
+	if (str == NULL)
+	{
+		str = "(nil)";
+	}
+	else
+		printf("%s", str);
+
+}
+/**
+ * print_int - prints integer
+ * @list: list of arguments
+ * Return: NULL
+ */
+
+void print_int(va_list list)
+{
+	int num;
+
+	num = va_arg(list, int);
+	printf("%d", num);
+}
+/**
+ * print_float - prints float numbers
+ * @list: list of arguments to be passed
+ * Return: NULL
+ */
+
+
+void print_float(va_list list)
+{
+	double fl = va_arg(list, double);
+
+	printf("%f", fl);
+}
+
+/**
+ * print_all - prints all formats
+ * @format: main string to be passed
+ * Return: NULL
+ */
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	char *separator;
+	int i;
+	int j;
+
+	spec_t type[] =  {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}
+	};
+
+	va_start(list, format);
+	/*iterate through the list access the functio*/
+
+	j = 0;
+	separator = " ";
+	while (format != NULL && format[j] != '\0')
+	{
+		i = 0;
+		while (type[i].letter != '\0')
+		{
+			if (type[i].letter == format[j])
+			{
+				printf("%s", separator);
+				type[i].func(list);
+				separator = ", ";
+			}
+			i++;
+		}
+		j++;
+	}
+	va_end(list);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,9 +4,25 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#include <unistd.h>
 
-int _putchar(char c);
+
+
+typedef struct type_spec
+{
+	char letter;
+	void (*func)(va_list);
+} spec_t ;
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+void print_all(const char * const format, ...);
+void print_string(va_list list);
+void print_char(va_list list);
+void print_int(va_list list);
+void print_float(va_list list);
+
 #endif /* _VARIADIC_FUNCTIONS_H */
