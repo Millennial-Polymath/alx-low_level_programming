5088d074f63dc05fb8da2fba219fddd67360cd12
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-20 00:36:11 +0000

    fixed some errors

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(list_s, char *);
 		if (str == NULL)
-			printf("nil");
+			printf("(nil)");
 		else
 			printf("%s", str);
 
