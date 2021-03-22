c57c6d2756e59748b42b7e6e9720ab654e2043ac
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-22 14:46:00 +0000

    Initial commit

---
+++ 0x13-more-singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t
+ * @h: pointer to the head node
+ * Return: Number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	const listint_t *current = h;
+
+	current = h;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		count++;
+	}
+
+	return (count);
+}
