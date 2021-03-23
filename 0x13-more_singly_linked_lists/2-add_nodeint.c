c4d0b8b8dd4785b86cd37c78047d04155a30af49
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-23 06:04:33 +0000

    Changed order of algorithm

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,10 +14,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new_node = (listint_t *)(malloc(sizeof(listint_t)));
 	new_node->n = n;
 	new_node->next = NULL;
-
-	new_node->next = *head;
 	if (head == NULL)
 		return (NULL);
+
+	new_node->next = *head;
+
+
 	*head = new_node;
 
 	return (*head);
