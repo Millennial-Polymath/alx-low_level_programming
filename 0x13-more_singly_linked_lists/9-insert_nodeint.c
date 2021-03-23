093bbd467a1842b59f976baef64a705d65dd7703
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-23 14:36:10 +0000

    fixed more errors

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new_node->next = *head;
 		*head = new_node;
 	}
-	else
+	else if (idx != 0)
 	{
 
 		while (temp != NULL && count < idx)
