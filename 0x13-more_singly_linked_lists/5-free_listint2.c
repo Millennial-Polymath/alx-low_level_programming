63d0eebdd33bf15086bdf00cf10ea13b5a83387e
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-23 09:17:49 +0000

    Handled case for an empty node

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,10 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current_node;
-
+	if (*head == NULL || head == NULL)
+	{
+		return;
+	}
 	while ((*head)->next != NULL)
 	{
 		current_node = (*head)->next;
