806388771676d89c998abeb69f7d30e86bfbb1e9
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-24 11:38:27 +0000

    Fixed error causing segmentation fault

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -11,16 +11,16 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-
 	if (head == NULL)
 		return;
 
-	while (head != NULL)
+
+	while (temp != NULL)
 	{
-		temp = head;
-		head = head->next;
-		free(temp);
+		temp = head->next;
+		free(head);
+		head = temp;
 
 	}
-	free(head);
+
 }
