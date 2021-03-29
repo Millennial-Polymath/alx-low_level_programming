d609fa0c6f9c73a0f5aa5401fda1f7cbaf223813
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-29 17:03:45 +0000

    Added files for task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * create_file - A function that creates a file
+ * @filename: The name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 upon success and -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_TRUNC, 0600);
+
+	if (fd < 0)
+		return (-1);
+
+
+	while (*(text_content + len) != '\0')
+		len++;
+
+	if (!len)
+		write(fd, "", len);
+	else
+		write(fd, text_content, len);
+
+	close(fd);
+
+	return (1);
+}
