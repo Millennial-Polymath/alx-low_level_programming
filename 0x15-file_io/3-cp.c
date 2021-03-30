14046799e2ec62f3578ce20ac99e220f681b8cdf
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-30 06:45:57 +0000

    changed the code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,45 +1,57 @@
 #include "holberton.h"
-
+#define BUFFER 1024
+#define PERMS 0664
 /**
- * main - copies the content of a file to another file
- *
- * @argc: argument count
+ * main - Program starts here
+ * @argc: number of arguments
  * @argv: argument vector
- * Return: 0 if successful or 97, 98, 99 or 100 if not
+ *
+ * Return: 0 upon success
  */
+
 int main(int argc, char *argv[])
 {
-	int fd_to, fd_from, b, w;
-	char buffer[1024];
+	int fd1, fd2, n;
+
+	char buf[BUFFER];
 
 	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
 	}
-	fd_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (fd_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	while ((b = read(fd_from, buffer, 1024)) > 0)
+
+
+
+	fd1 = open(argv[1], O_RDONLY, 0);
+
+	if (fd1 == -1)
 	{
-		w = write(fd_to, buffer, b);
-		if (w != b)
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
+	}
+
+
+	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, PERMS);
+
+
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
+	while ((n = read(fd1, buf, BUFFER)) > 0) /* cp: copy fd1 to fd2 */
+	{
+		if (write(fd2, buf, n) != n)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
 		}
 	}
-	if (b == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (close(fd_to) < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
-	if (close(fd_from) < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (n == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
+
+	if (close(fd1) != 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
+	if (close(fd2) != 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
+
+
 	return (0);
 }
