e16d7c39859961f25a45d6829d1c071daeebc121
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-30 06:34:50 +0000

    Fixed some errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,11 +29,16 @@ int main(int argc, char *argv[])
 		dprintf(2, "Error: Can't read from %s\n", argv[1]), exit(98);
 	}
 
-	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMS);
+
+	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, PERMS);
+
+
+	if (fd2 == -1)
+		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 
 	while ((n = read(fd1, buf, BUFFER)) > 0) /* cp: copy fd1 to fd2 */
 	{
-		if (fd2 < 0 || write(fd2, buf, n) != n)
+		if (write(fd2, buf, n) != n)
 		{
 			dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 
