a411869a6f33b2f2181a2641b83db100bb4dfcf9
Millennial-Polymath <muchamingotho@gmail.com>
2021-03-30 07:02:45 +0000

    Corrected betty related errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,30 +11,19 @@
 
 int main(int argc, char *argv[])
 {
-
-
 	int fd1, fd2, n;
-
 	char buf[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
-
-
-
 	fd1 = open(argv[1], O_RDONLY, 0);
-
 	if (fd1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
 	}
-
-
 	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-
-
 	if (fd2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
@@ -42,18 +31,20 @@ int main(int argc, char *argv[])
 	{
 		if (write(fd2, buf, n) != n)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
 		}
 	}
 	if (n == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
+		exit(98);
+	}
 
 	if (close(fd1) != 0)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
 	if (close(fd2) != 0)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
-
 
 	return (0);
 }
