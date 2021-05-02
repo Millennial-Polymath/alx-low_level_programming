cdcbb510acd5eae5b83f213c6e27bdac2df7d3e6
Millennial-Polymath <muchamingotho@gmail.com>
2021-05-02 08:17:56 +0000

    List length of node

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * dlistint_len - Finds the number of elements in a linked list
+ * @h: Pointer to the first node
+ *
+ * Return: Number of elements in a linked list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count;
+
+	while(h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
