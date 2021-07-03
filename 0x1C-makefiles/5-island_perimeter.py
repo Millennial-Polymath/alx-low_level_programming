5cb9cf8ac360b7b106779b8b4d7117d9d2041bac
Millennial-Polymath <muchamingotho@gmail.com>
2021-07-03 06:53:55 +0000

    Add docstrings

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 '''
-Module contains island_perimeter function
+Module contains: neighbours_no
+and island_perimeter function
 '''
 
 
 def neighbours_no(grid, y, x):
+    '''
+    neighbours_no - calculates the number of 1's sorrounding an element
+    Arguments:
+           grid - the 2d matrix
+           y - index for row(y axis)
+           x - index for column( x axis)
+    '''
     row = len(grid)
     column = len(grid[1])
     count = 0
@@ -29,6 +37,22 @@ def neighbours_no(grid, y, x):
 
 
 def island_perimeter(grid):
+    '''
+    island_perimeter - returns the perimeter of the island described in grid
+    argument(s):
+                grid - grid is a list of list of integers:
+                0 represents a water zone
+                1 represents a land zone
+                One cell is a square with side length 1
+                Grid cells are connected horizontally/vertically-
+                (not diagonally).
+                Grid is rectangular, width and height don’t exceed 100
+                Grid is completely surrounded by water,
+                and there is one island (or nothing).
+                The island doesn’t have “lakes”
+                (water inside that isn’t connected
+                 to the water around the island).
+    '''
     row = len(grid)
     column = len(grid[1])
     perimeter = 0
